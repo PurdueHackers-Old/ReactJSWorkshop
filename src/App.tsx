4f45251a4887d5b6ca4ae06366fbecbed8eb9086
Ashwin Gokhale <ashwin.gokhale98@gmail.com>
2019-01-27 18:51:10 -0500

    Added step 5

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import './App.css';
 import BooksPage from './containers/Books';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './containers/Home';
+import Navbar from './components/Navbar';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App App-header">
+				<Navbar />
+				<br />
 				<Switch>
 					<Route exact path="/" component={HomePage} />
 					<Route exact path="/books" component={BooksPage} />
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Navbar extends Component {
+	render() {
+		return (
+			<nav>
+				<Link to="/" className="App-link">
+					Home
+				</Link>{' '}
+				|{' '}
+				<Link to="/books" className="App-link">
+					Books
+				</Link>
+			</nav>
+		);
+	}
+}
