0b0e91171d70d8f816f4497508b1fc0f490cff01
Ashwin Gokhale <ashwin.gokhale98@gmail.com>
2019-01-27 18:36:05 -0500

    Added step 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import BooksPage from './containers/Books';
+import { Switch, Route } from 'react-router-dom';
+import HomePage from './containers/Home';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App App-header">
-				<BooksPage />
+				<Switch>
+					<Route exact path="/" component={HomePage} />
+					<Route exact path="/books" component={BooksPage} />
+				</Switch>
 			</div>
 		);
 	}
